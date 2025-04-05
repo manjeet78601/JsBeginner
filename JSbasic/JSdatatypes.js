18f430755031102af82a65a2f72b8649e72ce83e
manjeet78601 <47239230+manjeet78601@users.noreply.github.com>
2025-04-05 17:30:26 +0000

    datatypes code

---
+++ JSbasic/JSdatatypes.js
@@ -0,0 +1,27 @@
+"use Strict "; // treat All JS Code as newer version 
+
+// alert (3 + 3) we are using node js 
+
+console.log(3+
+    3) // code readability  should be high 
+ console.log("Manjeet")
+ let name ="manjeet"
+ let age=18
+ let isloggedIn= false
+ let state;
+
+ // number => 2 to power of 53 or 52
+ // bigint
+ //string => """
+ // "boolean => true/false
+ //null => standalone value 
+ // undefine =>  undefine 
+ // symbol => unique 
+
+ // 
+
+ // object 
+
+ console.log(typeof undefined);
+
+
