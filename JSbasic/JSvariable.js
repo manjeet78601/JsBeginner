ede03d031ebeffc87076b72ef4d7fd917b9b2174
manjeet78601 <47239230+manjeet78601@users.noreply.github.com>
2025-04-05 17:30:00 +0000

    variable code

---
+++ JSbasic/JSvariable.js
@@ -0,0 +1,26 @@
+const accountId= 244668
+let accountEmail="manjeet@hotmail.com"
+var accountPassword ="12345"
+accountCity="Jaipur"
+//accountId= 2   consttant value Not Allowed 
+ console.log(accountId);
+
+ accountEmail="man@gmail.com"
+ accountPassword="123456"
+ accountCity ="Pune"
+ let accounState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accounState])
+/* 
+Prefer not to use var 
+Because  of issue in block scope and function scope
+
+{
+variable is a scope 
+}
+*/
+
+
+
+
+
