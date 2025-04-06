c345495dc96ffc4ca2a51c40bb56b91aa0947ea7
manjeet78601 <47239230+manjeet78601@users.noreply.github.com>
2025-04-06 15:18:28 +0000

    conversion operator

---
+++ JSbasic/conversion_operation.js
@@ -0,0 +1,24 @@
+let score = "Manjeet" //string cannot be change but the out is also NAN   //false //true //undefined //null //33bgh
+//"33"=> 33
+//"33bgh"=> Nan (not a number )
+//true=> 1,0
+
+//  console.log(typeof score);
+//  console.log(typeof(score));
+ 
+ let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+ let isLoggedIn= "Manjeet"
+ let booleanIsLoggedIn = Boolean (isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+ //1=> true; 0=>false
+ //"" => false
+ //"Manjeet=>true"
+
+ let someNumber=35
+  let stringNumber= String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
