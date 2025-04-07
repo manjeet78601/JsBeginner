a1da09fd3bf050e5a03234d989c46216967b1940
manjeet78601 <47239230+manjeet78601@users.noreply.github.com>
2025-04-07 13:57:27 +0000

    operator cross value

--- JSbasic/conversion_operation.js
+++ JSbasic/conversion_operation.js
@@ -12,7 +12,7 @@ let score = "Manjeet" //string cannot be change but the out is also NAN   //fals
 
  let isLoggedIn= "Manjeet"
  let booleanIsLoggedIn = Boolean (isLoggedIn)
- console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
 
  //1=> true; 0=>false
  //"" => false
@@ -20,5 +20,43 @@ let score = "Manjeet" //string cannot be change but the out is also NAN   //fals
 
  let someNumber=35
   let stringNumber= String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+
+ // ********************************//
+
+ let value = 3
+ let negValue= -value
+//  console.log(negValue)
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3);
+//  console.log(2/3);
+//  console.log(2%3);
+
+let str1 = "welcome"
+let str2= "to Manjeet"
+let str3= str1+ str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+console.log(+true);
+console.log(+"");
+
+let num1, num2,num3
+num1 = num2 = num3 = 2 + 2  
+let gameCounter= 100
+++gameCounter;
+console.log(gameCounter);
+
+let x =5;
+const y = ++x;
+
+console.log(x);
+console.log(y);
+
+
+
