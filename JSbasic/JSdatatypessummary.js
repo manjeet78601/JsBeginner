17ae65aa4f80b2f071e97d9ea12548538df93089
manjeet78601 <47239230+manjeet78601@users.noreply.github.com>
2025-04-09 12:32:23 +0000

    stackheap

--- JSbasic/JSdatatypessummary.js
+++ JSbasic/JSdatatypessummary.js
@@ -35,3 +35,28 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+ const numbers = [1,2,3,4,5,]
+ console.log(numbers)
+
+ //++++++++++++++++++++++++
+
+ // Stack (Primitives), Heap(Non- Primitive)
+
+ let myYoutubename = "manjeetkadevroom"
+ let anothername ="manjeetkadevroom"
+ anothername= "welcome to my channel"
+
+
+ console.log(myYoutubename);
+ console.log(anothername);
+ let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
+
+ }
+ let userTwo = userOne
+ userTwo.email = "manjeet@google.com"
+ console.log(userOne.email);
+ console.log(userTwo.email);
+ 
