8c41a20f66b86beaca42936ed5543679693f3050
manjeet78601 <47239230+manjeet78601@users.noreply.github.com>
2025-04-12 07:56:08 +0000

    code push

---
+++ JSbasic/num_and_Maths.js
@@ -0,0 +1,13 @@
+const score =300
+console.log(score);
+const balance = new Number(2);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+const otherNumber=23.6678;
+console.log(otherNumber.toPrecision(4));
+const hundered = 1000000
+
+console.log(hundered.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++Maths Library with in javascript by default 
