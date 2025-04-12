9df7b85435cc17ea164cbfd4ffa89f6d41e0ed0b
manjeet78601 <47239230+manjeet78601@users.noreply.github.com>
2025-04-12 12:20:47 +0000

    mathrandom operator

--- JSbasic/num_and_Maths.js
+++ JSbasic/num_and_Maths.js
@@ -1,13 +1,27 @@
 const score =300
-console.log(score);
+//console.log(score);
 const balance = new Number(2);
-console.log(balance);
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+//console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 const otherNumber=23.6678;
-console.log(otherNumber.toPrecision(4));
+//console.log(otherNumber.toPrecision(4));
 const hundered = 1000000
 
-console.log(hundered.toLocaleString('en-IN'));
+//console.log(hundered.toLocaleString('en-IN'));
 
-//++++++++++++++++++++++++++++++Maths Library with in javascript by default 
+//++++++++++++++++++++++++++++++Maths Library with in javascript
+//  by default 
+ console.log(Math);
+ console.log(Math.abs(-4));// abs means absolute value
+ console.log(Math.round(4.6));
+
+ console.log(Math.random()); //( manth.random given value in 0 to 1))
+ console.log((Math.random()* 10) + 1);
+ console.log((Math.random()* 10) + 1);
+ console.log(Math.floor(Math.random()* 10) + 1);// lowest value consider approximate
+ 
+ const min=10
+ const max=20; 
+
+ console.log(Math.floor(Math.random() * (max - min + 1 )) + min ) //formula
