301597262cb8c1268a0e896bbc39faefe1b002fe
manjeet78601 <47239230+manjeet78601@users.noreply.github.com>
2025-04-22 06:29:39 +0000

    code push JSbasic

--- JSbasicloop/highorderarray.js
+++ JSbasicloop/highorderarray.js
@@ -41,12 +41,11 @@ for(const [key,value] of map){
     rb: "ruby",
     swift:"swift by apply"
 
- }
+}
  for(const key in  myObject1){
     console.log(`${key}shortcut is for ${myObject1[key]}`);
  }
  //for in loop
-
  const programing=["js", "rb","py","java","cpp"]
 
  for (const key in programing){
@@ -59,25 +58,26 @@ for(const [key,value] of map){
 const coding=["js", "rb","py","java","cpp"]
 
 coding.forEach( function (val){
-    console.log(val)
+    //console.log(val)
 }) // A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action.
                                     // there are two types of function synchronisation and asynchronisation
+
+                      
 
 
                                     // use arrow function with callback function 
 
                                     coding.forEach((item)=>{
-console.log(item);
+                                        console.log(item);
                                     })
 
 
                                     function printMe(item){
- console.log(item);
+                                    console.log(item);
                                     }
                                     coding.forEach(printMe)
-
                                     coding.forEach((item, index,arr)=>{
-                                        console.log(item,index,arr);
+                                    console.log(item,index,arr);
                                     })
 const myCoding=[
     {
@@ -98,7 +98,50 @@ const myCoding=[
 
 ]
 myCoding.forEach((item)=>{
-    console.log(item.languageName);
+   // console.log(item.languageName);
 })
 
 
+/// for each loop  
+
+// const coding1=["js", "rb","py","java","cpp"]
+// const values= coding.forEach((item)=>{  
+//                                         //for each value ko return nahi krta 
+//    // console.log(item);
+//     return item 
+// })
+// console.log(values)
+
+
+//using filter method  
+// Filter element based on condition return a new array 
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+ const newNums=myNums.filter ((num)=>{
+   return num > 4
+})  
+                                      //for each value ko return nahi krta 
+    console.log(newNums);
+
+//use For Each  use 
+const newNums1=[]
+myNums.forEach((num)=>{
+    if(num>4){
+        newNums.push(num)
+    }
+})
+console.log(newNums1)
+// example for books
+
+const books=[
+{title:'BookOne', genre: 'Fiction',publish:1981, edition:2004},
+ {title:'Boottwo', genre: 'Non-Fiction',publish:1982, edition:2005},
+{title:'Bookthree', genre: 'History',publish:1983, edition:2006},
+{title:'Bookfour', genre: 'Fiction4',publish:1984, edition:2007},
+{title:'Bookfive', genre: 'History',publish:1985, edition:2008},
+{title:'Booksix', genre: 'Fiction6',publish:1986, edition:2009},
+{title:'Bookseven', genre: 'History',publish:1987, edition:2010},
+]
+
+let userBooks =books.filter((bk)=>bk.genre==='History')
+ userBooks = books.filter((bk)=>{ return bk.publish>=1985})
+console.log(userBooks)
